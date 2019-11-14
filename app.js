c524ab915074e9716546a6131e3c018680add8f9
William Xu <williamxu97@gmail.com>
2019-11-14 12:45:12 -0500

    setup database

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const pg = require('pg');
 const routes = require('./router');
-
+const dbObjects = require('./models');
+const Sequelize = require('sequelize');
+const db = dbObjects.db;
+const User = dbObjects.User;
+const Page = dbObjects.Page;
 
 const app = express();
 
 app.use(routes);
 
-
-app.listen(3000, ()=>{
-	console.log('listening on port 3000')
-});
+const init = async () => {
+  await db.sync(/*{ force: true }*/);
+  app.listen(3000, () => {
+    console.log('listening on port 3000');
+  });
+};
+init();
---
+++ models/index.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
+
+const Page = db.define('page', {
+  title: { type: Sequelize.STRING, allowNull: false },
+  slug: { type: Sequelize.STRING, allowNull: false },
+  content: { type: Sequelize.TEXT, allowNull: false },
+  status: { type: Sequelize.STRING, allowNull: false },
+});
+
+const User = db.define('user', {
+  name: { type: Sequelize.STRING, allowNull: false },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: { isEmail: true },
+  },
+});
+
+module.exports = { db, Page, User };
